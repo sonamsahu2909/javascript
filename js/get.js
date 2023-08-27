6d6d20caea12864b35a5077048fdf7ca28712897
sonamsahu2909 <sonamsahu2909@gmail.com>
2023-08-27 15:55:15 +0530

    send

---
+++ js/get.js
@@ -0,0 +1,9 @@
+//document.write("hello mits")
+//let data=document.getElementById("a").innerText;
+//let data=document.getElementById("a").innerHTML;
+// // data=document.getElementById("aa").innerHTML;
+//let data=document.getElementById("a").getAttribute("style");
+data=document.getElementById("a").getAttributeNode("style");
+//data=document.getElementById("a").attributes[0];
+
+console.log(data);
---
+++ js/set.js
@@ -0,0 +1,4 @@
+document.getElementById("a").innerHTML = "<center><h1>PNINFOSY Delhi</h1></center>";
+document.getElementById("a").setAttribute("style","border:10px dotted red");
+//document.getElementById("a").removeAttribute("style");
+document.getElementById("b").innerHTML = "<h1>GWALIOR</h1>";
